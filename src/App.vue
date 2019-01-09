60bccdbd9da22f343bb8c73740f59cf0295c35a1
Erdem <etc.turgut@gmail.com>
2019-01-09 21:44:40 +0100

    deleted todo components and reset file to original form

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,18 @@
 <template>
   <div id="app">
-    <h1 class="ui dividing centered header">Vue:js Todo App</h1>
-    <div class="ui three column centered grid">
-      <div class="column">
-        <img src="./assets/logo.png">
-        <router-view/>
-        <todo-list v-bind:todos="todos"></todo-list>
-        <create-todo v-on:create-todo="createTodo"></create-todo>
-      </div>
-    </div>
+    <img src="./assets/logo.png">    
+    <router-view/>
   </div>
 </template>
 
 <script>
-import sweetalert from 'sweetalert'
-import TodoList from './components/TodoList'
-import CreateTodo from './components/CreateTodo'
-
 export default {
   name: 'App',
-
-  components: {
-    TodoList,
-    CreateTodo,
-  },
-
-  data() {
-    return {
-      todos: [
-        {
-          title: 'Todo A',
-          project: 'Project A',
-          done: false,
-        },
-        {
-          title: 'Todo B',
-          project: 'Project B',
-          done: true,
-        },
-        {
-          title: 'Todo C',
-          project: 'Project C',
-          done: false,
-        },
-      ]
-    };
-  },
-
-  methods: {
-    createTodo(newTodo) {
-      this.todos.push(newTodo);
-      sweetalert('Erfolgreich!', 'To-Do eingerichtet!', 'Erfolgreich');
-    }
-  }
 
 }
 </script>
 
 <style>
-/* 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -67,5 +21,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-*/
 </style>
