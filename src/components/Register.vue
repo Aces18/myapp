ffcda9a114f0f2af6b00bd0b7ee80d9dce72d3bd
Erdem <etc.turgut@gmail.com>
2019-01-18 22:37:31 +0100

    modify tool

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,13 +1,10 @@
 <template>
     <div>
         <h1>Register</h1>
-
         <input type="email" name="email" v-model="email" placeholder="email"/>
         <br>
         <input type="password" name="password" v-model="password" placeholder="password"/>
         <br>
-        <button>Register</button>
-        <button>Setup</button>
     </div>
 </template>
 
