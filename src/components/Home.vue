ca7189e0c84c0b5507cd5b65e037c261204f657f
Erdem <etc.turgut@gmail.com>
2019-01-18 23:50:33 +0100

    added home component

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <b-navbar toggleable type="light" variant="light">
+      <b-navbar-toggle target="nav_text_colapse"></b-navbar-toggle>
+      <b-navbar-brand>myapp</b-navbar-brand>
+      <b-collapse is-nav id="nav_text_collapse">
+        <b-navbar-nav>
+          <b-nav-text>Home</b-nav-text>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Willkommen im Lappenexpress'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
